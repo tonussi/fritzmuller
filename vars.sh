36406580d2becafdc39f51446bd1ebed84ce543b
tonussi <lucas.tonussi@agrosatelite.com.br>
2018-10-16 15:16:26 -0300

    Add .env.example

--- vars.sh
+++ vars.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 heroku config:set ALGOLIA_APP_ID=CBRX9GBWPS
 heroku config:set ALGOLIA_SECRET=bc2131413036bc5d9dcec38335daf6ff
+
 heroku config:set APP_URL=fritzmuller.herokuapp.com
 
 heroku config:set GOOGLE_RECAPTCHA_KEY=6LdgDBYTAAAAAN6RpxiDWiK8GML7LaUdNZHrQLWS
 heroku config:set GOOGLE_RECAPTCHA_SECRET=6LdgDBYTAAAAAPqtLzPREOyDsjQAeR9ABE_rOkqf
 
 heroku config:set JWT_SECRET=cg4IFk1STdOf1XhsrprcHDSzsvQHrpj0
-
-heroku config:set DATABASE_HOST=ec2-50-17-194-186.compute-1.amazonaws.com
-heroku config:set DATABASE_NAME=d3sg183uljpduk
-heroku config:set DATABASE_USER=svicnvvdgwmdfn
-heroku config:set DATABASE_PASS=0c133b9068e01843e78b5d228883a62c907d504f5c0eb76898d6c34565ebf2bb
