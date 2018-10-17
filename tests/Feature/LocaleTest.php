9d5d301d1deecf792ebd74e31282e91f05c96111
tonussi <lucas.tonussi@agrosatelite.com.br>
2018-10-17 19:56:34 -0300

    Change test

--- tests/Feature/LocaleTest.php
+++ tests/Feature/LocaleTest.php
@@ -9,10 +9,10 @@ class LocaleTest extends TestCase
     /** @test */
     public function set_locale_from_header()
     {
-        $this->withHeaders(['Accept-Language' => 'zh-CN'])
+        $this->withHeaders(['Accept-Language' => 'pt-BR'])
             ->postJson('/api/login');
 
-        $this->assertEquals('zh-CN', $this->app->getLocale());
+        $this->assertEquals('pt-br', $this->app->getLocale());
     }
 
     /** @test */
