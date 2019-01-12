db1d1fae3132f25ee0346abb51224e0f6d9d4ede
tonussi <lptonussi@gmail.com>
2019-01-12 15:14:35 -0200

    Change description meta tag

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -21,7 +21,7 @@ $polyfills = [
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 
-  <meta name="Description" CONTENT="Author: Maria Cristina Pagotto Tonussi, Illustrator: Maria C. P. T. et al., Category: Educational, About: Fritz Muller ponto Org. Esse site é uma uma simples homenagem ao legado científico">
+  <meta name="Description" content="Author: Maria Cristina Pagotto Tonussi, Illustrator: Maria C. P. T. et al., Category: Educational, Area: Plants and Biology, About: Fritz Müller ponto Org. Esse site é uma uma simples homenagem ao legado científico de Fritz Müller.">
 
   <title>{{ config('app.name') }}</title>
 
