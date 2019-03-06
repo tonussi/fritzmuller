97970e281062848cdad99f808ed909ecbb5ba067
tonussi <lptonussi@gmail.com>
2019-03-06 02:33:44 -0300

    Finish searcher and regex

--- app/Http/Controllers/Api/V1/BritannicaService.php
+++ app/Http/Controllers/Api/V1/BritannicaService.php
@@ -6,26 +6,25 @@ class BritannicaService extends Controller
 {
     public function index($taxon, $limit = 10)
     {
-        $matches_more_info = array();
-        $matches_photos = array();
         $url = "https://www.britannica.com/search?query=" . urlencode($taxon);
+        
+        $matches_more_info = array();
         $str = CurlHelper::get_remote_data($url);
-        $re_more_info = '/\/([a-zA-Z0-9\-]+)\/([a-zA-Z0-9\-]+)\/(image\-[a-zA-Z0-9\-]+)/m';
-        $re_photos = '/[https\:\/\/]([^\"\']*\.jpe?g)/';
+        $re_more_info = "/(https\:\/\/www\.britannica\.com\/animal\/[\s-()a-zA-Z0-9]+)/";
         preg_match_all($re_more_info, $str, $matches_more_info, PREG_OFFSET_CAPTURE);
-        preg_match_all($re_photos, $str, $matches_photos, PREG_OFFSET_CAPTURE);
         $matches_more_info = array_slice($matches_more_info[0], 0, $limit);
+        
+        $matches_photos = array();
+        $str2 = CurlHelper::get_remote_data($matches_more_info[0][0]);
+        $re_more_info2 = "/(https\:\/\/cdn\.britannica\.com\/\d+\/[\s-()a-zA-Z0-9.jpg]+)/";
+        preg_match_all($re_more_info2, $str2, $matches_photos, PREG_OFFSET_CAPTURE);
         $matches_photos = array_slice($matches_photos[0], 0, $limit);
-        $arr = array();
-        foreach ($matches_more_info as $key => $value) {
-            try {
-                array_push($arr, ['img' => $matches_photos[$key][0],
-                    'info' => 'https://www.britannica.com' . $matches_more_info[$key][0]
-                ]);
-            } catch (\Throwable $th) {
-                continue;
-            }
+
+        for ($i=0; $i < sizeof($matches_photos); $i++) {
+            $matches_photos[$i]['img'] = $matches_photos[$i][0];
+            $matches_photos[$i]['info'] = $matches_more_info[0][0];
         }
-        return $arr;
+
+        return $matches_photos;
     }
 }
